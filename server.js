a06836bd7a494ed07c5df5645cfab709a0a12ab1
pfrimpong11 <frimpongprince2000@gmail.com>
2024-09-30 13:49:51 +0000

    port update

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.use('/api/messages', messageRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
